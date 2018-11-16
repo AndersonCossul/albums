c736a1e21eb6b38896b19eaf77575463a77613bc
Anderson Cossul <anderson_cossul@hotmail.com>
2018-11-16 21:34:07 -0200

    Added eslint.

--- App.js
+++ App.js
@@ -12,9 +12,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu\n' +
-    'Finally this thing is working!!!',
+    'It\'s working, at least.',
 });
 
 type Props = {};
@@ -22,8 +20,6 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
@@ -38,7 +34,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 25,
     textAlign: 'center',
     margin: 10,
   },
