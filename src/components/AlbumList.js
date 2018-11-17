31c5a3d56d60a12b4bb569c4907ac8722ce98269
Anderson Cossul <anderson_cossul@hotmail.com>
2018-11-16 23:35:28 -0200

    Fetching albums from api.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => (
-  <View>
-    <Text>Album List!</Text>
-  </View>
-);
+class AlbumList extends React.Component {
+  state = {
+    albums: []
+  }
+
+  componentWillMount = () => {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => {
+        this.setState({ albums: response.data });
+      });
+  }
+
+  renderAlbums = () => {
+    const { albums } = this.state;
+    return albums.map((album, index) =>
+      <Text key={index}>{album.title}</Text>
+    );
+  }
+
+  render() {
+    return this.renderAlbums();
+  }
+}
 
 export default AlbumList;
