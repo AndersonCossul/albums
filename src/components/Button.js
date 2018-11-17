581732cc91b65ab3997c4c41de3bdaa36e9de5ea
Anderson Cossul <anderson_cossul@hotmail.com>
2018-11-17 00:48:23 -0200

    Opening url.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
 const Button = (props) => (
-  <TouchableOpacity style={styles.button}>
+  <TouchableOpacity
+    style={styles.button}
+    onPress={props.onPress}
+  >
     <Text style={styles.text}>{props.children}</Text>
   </TouchableOpacity>
 );
